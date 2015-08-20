c66bc63a69d9016cc2395bc6449e2df94b06a187
yoyo930021 <yoyo930021@gmail.com>
2015-08-20 22:15:34 +0800

    錯誤與維修頁面修改

--- app/views/close.blade.php
+++ app/views/close.blade.php
@@ -10,7 +10,7 @@
             <br>
             <h2 class="ui blue image header">
                 <div class="content">
-                    <i class="arrow down icon" style="font-size: 8em;"></i>
+                    <i class="arrow down icon" style="font-size: 5em;"></i><br>
                     系統維護中<br>請稍後在來訪 謝謝！
                 </div>
             </h2>
