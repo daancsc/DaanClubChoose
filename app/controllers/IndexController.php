bf2bc98059a74245d58d9b4e7be77dd30381e20b
daancsc <daancsc@gmail.com>
2015-08-21 08:40:51 +0800

    瀏覽器友善提示修正

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -6,7 +6,7 @@ class IndexController extends BaseController {
         $browser = Agent::browser();
         $version = Agent::version($browser);
         if($browser=="IE"&&$version<10){
-            dd("<script>alert('請使用IE10以上瀏覽器預覽/r/n或其他瀏覽器如：chrome,firefox,opera,safari');document.location.href='https://whatbrowser.org/';</script>");
+            dd("<script>alert('請使用IE10以上瀏覽器預覽/r/n或其他瀏覽器如：chrome,firefox,opera,safari/r/n或改用智慧型手機填寫');document.location.href='https://whatbrowser.org/';</script>");
         }
         $status=Settings::where('item', 'status')->first();
         $important=Settings::where('item', 'post_important')->first();
